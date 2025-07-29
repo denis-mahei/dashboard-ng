d78b2ea6f2a25b6650f1ccb7cd28fbaf0ce3b87a
denis-mahei <denmahei@gmail.com>
2025-07-29 20:16:50 +0300

    done task 1

--- src/app/task2/task2.component.ts
+++ src/app/task2/task2.component.ts
@@ -1,11 +1,13 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import friends from '../../data/friends-data.json';
 
 @Component({
   selector: 'app-task2',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './task2.component.html',
-  styleUrl: './task2.component.scss'
+  styleUrl: './task2.component.scss',
 })
 export class Task2Component {
-
+  data = friends;
 }
